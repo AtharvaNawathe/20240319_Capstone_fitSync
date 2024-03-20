c2fd6f58a10f59497b05659f6f0ccfca56c5f850
Atharva <atharvanhsm@gmail.com>
2024-03-20 12:42:22 +0530

    Added API, Wireframes,html pages

---
+++ 02_UX/assets/js/script.js
@@ -0,0 +1,34 @@
+const navbar = document.querySelector(".navbar");
+const menuToggle = document.querySelector(".menu-toggle");
+
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("open");
+});
+
+/*================================================
+    Nav Button
+=================================================*/
+
+//Get the Nav Button \\
+const mybutton = document.getElementById("navbutton");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+/*================================================
+    ScrollDown
+=================================================*/
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
